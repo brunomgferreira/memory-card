1822cf867d3b933b381c06cf374180a060211607
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-09-16 18:57:41 +0100

    design done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Scoreboard from './Scoreboard/Scoreboard.jsx'
+import CardsGrid from './Cardsgrid/CardsGrid.jsx'
+import { capitalizeFirstLetter, shuffleArray } from '../utils'
 
 const Main = () => {
+
   return (
-    <div>Main</div>
+    <MainWrapper>
+      <Scoreboard
+        currentScore={currentScore}
+        bestScore={bestScore}
+      ></Scoreboard>
+      <CardsGrid
+        pokemons={pokemons}
+        handleCardClick={handleCardClick}
+      ></CardsGrid>
+    </MainWrapper>
   )
 }
+
+const MainWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  padding: 4rem;
+  margin-bottom: 3.5rem;
+`
 
 export default Main
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './theme/GlobalStyle'
+import Theme from './theme/Theme'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
