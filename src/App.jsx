5f66ac4cd7c7c08bd645cd9da73a6691bd4a3350
Bruno Ferreira <brunomgferreira17@gmail.com>
2023-09-16 14:57:42 +0100

    Changed the structure of files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
-      <p>Memory Card</p>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
